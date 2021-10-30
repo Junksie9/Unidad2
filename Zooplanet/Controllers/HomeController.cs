82128473e962babc3d1b4a0bdb9ef1c0e6a750a2
Junksie9 <89873667+Junksie9@users.noreply.github.com>
2021-10-29 20:12:46 -0500

    Agregar archivos de proyecto.

---
+++ Zooplanet/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Zooplanet.Models;
+
+namespace Zooplanet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        [Route("Segun-Su-Clase")]
+        public IActionResult Clases()
+        {
+            
+            animalesContext context = new animalesContext();
+            var clas = context.Clases.Include(x => x.Especies).OrderBy(x => x.Nombre);
+            if (clas == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(clas);
+        }
+
+        [Route("Segun-Su-Habitat")]
+        public IActionResult Habitat()
+        {
+
+            animalesContext context = new animalesContext();
+            var habi = context.Especies.Include(x => x.IdClaseNavigation).OrderBy(x => x.Habitat);
+
+
+            if (habi == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(habi);
+        }
+    }
+}
